99eaf481e34e37aed2f2f5be33eb15b19a9f90ef
shinying <shinying.lees@gmail.com>
2019-05-21 18:00:24 +0800

    test template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, url_for
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -7,7 +7,8 @@ from linebot.models import *
 import os
 
 app = Flask(__name__)
-base_url = 'https://fake-news-linebot.herokuapp.com/files/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)))
+ASS_DIR = os.path.join(BASE_DIR, 'assets')
 
 line_bot_api = LineBotApi('pDq55Cpri+YlG3ADO7C++ufKY0ouqcXNOATku0+0RwIMYvrnOfZPaMfjznfHyGGzbfn53zny2NEOQLWdxDnsaX3yHSDjkFv9gPmX1hD6a+5tVTjOMdwlBk5rScGo1zl5vsN24KgPscitpHF4X4rXywdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('13a1e740fbc927e2955de31fc7552fbd')
@@ -33,7 +34,7 @@ def handle_message(event):
         message = TemplateSendMessage(
             alt_text='男歌手',
             template=ButtonsTemplate(
-                thumbnail_image_url=base_url+'m_singer.jpg',
+                thumbnail_image_url=url_for('assets', filename='m_singer.jpg'),
                 title='小宇',
                 text='台灣少見的奇幻風格與前衛曲風，是全方位的音樂人',
                 actions=[
@@ -47,8 +48,8 @@ def handle_message(event):
                         text='message text'
                     ),
                     URITemplateAction(
-                        label='uri',
-                        uri='http://example.com/'
+                        label='Youtube Channel',
+                        uri='https://www.youtube.com/channel/UC8C3GSHSFQltb9RjbCGgmPA'
                     )
                 ]
             )
