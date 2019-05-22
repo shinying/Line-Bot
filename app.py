57b9340f47f3e011a2f6084129cbc8124c78db0f
shinying <shinying.lees@gmail.com>
2019-05-22 11:53:49 +0800

    add many problems

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
+from cv import *
+from myself import *
+
 import os
 
 app = Flask(__name__)
@@ -28,33 +31,65 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    if '男' in text and '歌手' in text:
-        print(url_for('static', filename='m_singer.jpg'))
+    if '常見問題' in text:
+        message = get_basic_ques()
+    elif '自我介紹' in text:
+        message = TextSendMessage(text="我從資訊技術出發，遊走於心理、設計與管理之間，致⼒於將以⼈為本的設計原則注入科技領域，並應⽤科技解決問題。")
+    elif '錄取' in text:
+        message = TextSendMessage(text="好的工程師除了要有紮實的技術硬底子，也需要有良好的團隊合作能力。在資：管 = 7:3系上課程和課堂外project的訓練下必備的能力我不會少，而我還有很多社團和校外組織的跨領域合作經驗，在溝通和表達上更具優勢。")
+    elif '申請' in text:
+        message = TextSendMessage(text="Line不但是台灣市佔率最高的通訊軟體，也是積極開拓新服務和跨國網路和軟體公司，我很想試試和國際級的人才並肩工作的感覺！")
+    elif '生活' in text:
+        message = TextSendMessage(text="你可以問問我喜歡的歌手、運動、動物，或參加過的社團")
+    elif ('喜歡' in text or '喜愛' in text):
+        if '歌手' in text:
+            message = get_favor('singer')
+        elif '動物' in text:
+            message = get_favor('animal')
+        elif '運動' in text:
+            message = get_favor('sport')
+        else:
+            message = TextSendMessage(text="其他喜歡的東西還沒有加入喔～")
+    elif '社團':
+        message = get_clubs()
+    elif '技能' in text:
+        message = get_skills()
+    elif '專題' in text:
+        message = get_projects()
+    elif '比賽' in text:
+        message = get_contests()
+    else:
         message = TemplateSendMessage(
-            alt_text='男歌手',
+            alt_text='unknown',
             template=ButtonsTemplate(
-                thumbnail_image_url="https://i.imgur.com/RwJ9oIW.jpg",
-                title='小宇',
-                text='台灣少見的奇幻風格與前衛曲風，是全方位的音樂人',
+                # thumbnail_image_url='',
+                title='抱歉我沒有聽懂',
+                text='不過也許我們可以聊聊',
                 actions=[
-                    PostbackTemplateAction(
-                        label='postback',
-                        text='postback text',
-                        data='action=buy&itemid=1'
+                    MessageTemplateAction(
+                        label='常見問題',
+                        text='常見問題'
                     ),
                     MessageTemplateAction(
-                        label='message',
-                        text='message text'
+                        label='專題',
+                        text='專題'
                     ),
-                    URITemplateAction(
-                        label='Youtube Channel',
-                        uri='https://www.youtube.com/channel/UC8C3GSHSFQltb9RjbCGgmPA'
-                    )
+                    MessageTemplateAction(
+                        label='比賽',
+                        text='比賽'
+                    ),
+                    MessageTemplateAction(
+                        label='技能',
+                        text='技能'
+                    ),
+                    MessageTemplateAction(
+                        label='生活小事',
+                        text='生活小事'
+                    ),
                 ]
             )
         )
-    else:
-        message = TextSendMessage(text=event.message.text)
+
     line_bot_api.reply_message(event.reply_token, message)
 
 
---
+++ cv.py
@@ -0,0 +1,103 @@
+from linebot.models import *
+
+
+def get_skills():
+    message = TextSendMessage(text="最熟悉的語言是 Python 和 C++，其他如資料分析、機器學習、區塊鏈等等熱門關鍵字都了解一點。")
+    return message
+
+def get_projects():
+    message = TemplateSendMessage(
+        alt_text='projects',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='',
+                    title='Dance Light',
+                    text='幫助聽障舞者感受音樂的燈光裝置',
+                    actions=[
+                        URITemplateAction(
+                            label='watch video',
+                            uri='https://www.youtube.com/watch?v=Yx0Welpq9NY'
+                        ),
+                        URITemplateAction(
+                            label='read document',
+                            uri='https://drive.google.com/open?id=110YDLC42UPF5E4cwvmgw3WnwZKq_24PJ'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='',
+                    title='高雄港智慧拖船調度演算法',
+                    text='應用最佳化方法和訓練拖船工作時間預測模型來自動化高雄港的拖船調度，並開發模擬系統，以港區會發生的真實事件評估效能',
+                    actions=[
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/fzRDFyE.png',
+                    title='NodEverywhere',
+                    text='',
+                    actions=[
+                        URITemplateAction(
+                            label='read document',
+                            uri='https://drive.google.com/open?id=1lqL_Yfh9MgvPunjNDeRqS8M25sZeHcF2'
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    return message
+
+
+def get_contests():
+    message = TemplateSendMessage(
+        alt_text='contests',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/FOGaRWQ.png',
+                    title='Addon',
+                    text='2018臺大、師大、臺科大三校 App 創意競賽冠軍。以手機端和網頁打造無紙化的 brainstorming 體驗',
+                    actions=[
+                        URITemplateAction(
+                            label='watch video',
+                            uri='https://www.youtube.com/watch?v=IdpYBQ3P_fU&t=14s'
+                        ),
+                        URITemplateAction(
+                            label='visit Github',
+                            uri='https://github.com/shinying/Addon'
+                        ),
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/S0vtWir.png',
+                    title='ChainSurance',
+                    text='第二屆上海銀行滬港台金融科技創新校園競賽第四名。為小型商家、個人勞動者創造全新商業模式。',
+                    actions=[
+                        URITemplateAction(
+                            label='read slides',
+                            uri='https://drive.google.com/file/d/1CG7wkHzd3X6hhQoxYbBwdnXbccAOee_z/view'
+                        ),
+                    ]
+                )
+            ]
+        )
+    )
+    return message
+
+def get_clubs():
+    message = TemplateSendMessage(
+        alt_text='club',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://i.imgur.com/YBw7dzA.jpg',
+            title='不一樣思考社',
+            text='我大一二在不一樣思考社擔任課程長，致力於推廣和應用設計思考。另外也參加過阿卡貝拉社、NTU Maker等社團',
+            actions=[
+                URITemplateAction(
+                    label='Facebook',
+                    uri='https://www.facebook.com/NTUd.thinking/'
+                ),
+            ]
+        )
+    )
+    return message
