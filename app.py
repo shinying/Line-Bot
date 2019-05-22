f090c5c5fa6816e3b91f5c0c03724bcc9e822631
shinying <shinying.lees@gmail.com>
2019-05-22 15:53:49 +0800

    fix re

--- app.py
+++ app.py
@@ -42,32 +42,32 @@ def handle_message(event):
         message = get_basic_ques()
     elif re.match('自我介紹', text):
         message = TextSendMessage(text="我從資訊技術出發，遊走於心理、設計與管理之間，致⼒於將以⼈為本的設計原則注入科技領域，並應⽤科技解決問題。")
-    elif re.match('為什麼.*錄取', text):
+    elif re.match('.*為什麼.*錄取', text):
         message = TextSendMessage(text="好的工程師除了要有紮實的技術硬底子，也需要有良好的團隊合作能力。在資：管 = 7:3系上課程和課堂外project的訓練下必備的能力我不會少，而我還有很多社團和校外組織的跨領域合作經驗，在溝通和表達上更具優勢。")
-    elif re.match('為什麼.*申請', text):
+    elif re.match('.*為什麼.*申請', text):
         message = TextSendMessage(text="Line不但是台灣市佔率最高的通訊軟體，也是積極開拓新服務和跨國網路和軟體公司，我很想試試和國際級的人才並肩工作的感覺！")
-    elif re.match('興趣', text):
+    elif re.match('.*興趣', text):
         message = TextSendMessage(text="唱歌吧，你還可以問問我喜歡的歌手、運動、動物，或參加過的社團")
     elif re.match('[^不無]*[最喜歡愛]', text):
-        if re.match('歌手', text):
+        if re.match('.*歌手', text):
             message = get_favor('singer')
-        elif re.match('動物', text):
+        elif re.match('.*動物', text):
             message = get_favor('animal')
-        elif re.match('運動', text):
+        elif re.match('.*運動', text):
             message = get_favor('sport')
         else:
             message = TextSendMessage(text="其他喜歡的東西還沒有加入喔～")
-    elif re.match('錄取', text):
+    elif re.match('.*錄取', text):
         message = get_clubs()
-    elif re.match('技能', text):
+    elif re.match('.*技能', text):
         message = get_skills()
-    elif re.match('專題', text):
+    elif re.match('.*專題', text):
         message = get_projects()
-    elif re.match('比賽', text):
+    elif re.match('.*比賽', text):
         message = get_contests()
-    elif re.match('修.*課', text):
+    elif re.match('.*修.*課', text):
         message = TextSendMessage(text="系上的課程規劃是資:管 7:3，除了資工基本的線代、資結、演算法等，也有管理相關的統計、會計。另外我也選修了其他如區塊鏈、語音處理、機器學習等課程。")
-    elif re.match('開.*話題', text):
+    elif re.match('.*開.*話題', text):
         topics = pick(1)
         message = TemplateSendMessage(
             alt_text='unknown',
