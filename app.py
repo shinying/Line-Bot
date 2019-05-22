b6d5620ed704ebbaf35290f6f0324fe6cb7fe273
shinying <shinying.lees@gmail.com>
2019-05-22 15:46:58 +0800

    add pick topic

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from cv import *
 from myself import *
 
 import os
+import random
+import re
 
 app = Flask(__name__)
 
@@ -28,59 +30,77 @@ def callback():
     return 'OK'
 
 
+def pick(n):
+    topics = ['常見問題', '興趣', '參加過的社團', '技能', '專題', '參加過的比賽', '修過什麼課']
+    return random.choices(topics, k=n)
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    if '常見問題' in text:
+    if re.match('常見問題', text):
         message = get_basic_ques()
-    elif '自我介紹' in text:
+    elif re.match('自我介紹', text):
         message = TextSendMessage(text="我從資訊技術出發，遊走於心理、設計與管理之間，致⼒於將以⼈為本的設計原則注入科技領域，並應⽤科技解決問題。")
-    elif '錄取' in text:
+    elif re.match('為什麼.*錄取', text):
         message = TextSendMessage(text="好的工程師除了要有紮實的技術硬底子，也需要有良好的團隊合作能力。在資：管 = 7:3系上課程和課堂外project的訓練下必備的能力我不會少，而我還有很多社團和校外組織的跨領域合作經驗，在溝通和表達上更具優勢。")
-    elif '申請' in text:
+    elif re.match('為什麼.*申請', text):
         message = TextSendMessage(text="Line不但是台灣市佔率最高的通訊軟體，也是積極開拓新服務和跨國網路和軟體公司，我很想試試和國際級的人才並肩工作的感覺！")
-    elif '興趣' in text:
+    elif re.match('興趣', text):
         message = TextSendMessage(text="唱歌吧，你還可以問問我喜歡的歌手、運動、動物，或參加過的社團")
-    elif ('喜歡' in text or '喜愛' in text):
-        if '歌手' in text:
+    elif re.match('[^不無]*[最喜歡愛]', text):
+        if re.match('歌手', text):
             message = get_favor('singer')
-        elif '動物' in text:
+        elif re.match('動物', text):
             message = get_favor('animal')
-        elif '運動' in text:
+        elif re.match('運動', text):
             message = get_favor('sport')
         else:
             message = TextSendMessage(text="其他喜歡的東西還沒有加入喔～")
-    elif '社團' in text:
+    elif re.match('錄取', text):
         message = get_clubs()
-    elif '技能' in text:
+    elif re.match('技能', text):
         message = get_skills()
-    elif '專題' in text:
+    elif re.match('專題', text):
         message = get_projects()
-    elif '比賽' in text:
+    elif re.match('比賽', text):
         message = get_contests()
-    else:
+    elif re.match('修.*課', text):
+        message = TextSendMessage(text="系上的課程規劃是資:管 7:3，除了資工基本的線代、資結、演算法等，也有管理相關的統計、會計。另外我也選修了其他如區塊鏈、語音處理、機器學習等課程。")
+    elif re.match('開.*話題', text):
+        topics = pick(1)
         message = TemplateSendMessage(
             alt_text='unknown',
             template=ButtonsTemplate(
-                # thumbnail_image_url='',
+                title='開個話題',
+                text='也許我們可以來聊聊',
+                actions=[
+                    MessageTemplateAction(
+                        label=topics[0],
+                        text=topics[0]
+                    )
+                ]
+            )
+        )
+    else:
+        topics = pick(3)
+        message = TemplateSendMessage(
+            alt_text='unknown',
+            template=ButtonsTemplate(
                 title='抱歉我沒有聽懂',
                 text='不過也許我們可以聊聊',
                 actions=[
                     MessageTemplateAction(
-                        label='常見問題',
-                        text='常見問題'
+                        label=topics[0],
+                        text=topics[0]
                     ),
                     MessageTemplateAction(
-                        label='專題',
-                        text='專題'
+                        label=topics[1],
+                        text=topics[1]
                     ),
                     MessageTemplateAction(
-                        label='比賽',
-                        text='比賽'
-                    ),
-                    MessageTemplateAction(
-                        label='技能',
-                        text='技能'
+                        label=topics[2],
+                        text=topics[2]
                     ),
                 ]
             )
