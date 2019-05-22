8c3b1a760785082a619397277f02ffe53f21c263
shinying <shinying.lees@gmail.com>
2019-05-22 17:56:47 +0800

    Really fix all emojis

--- app.py
+++ app.py
@@ -39,15 +39,15 @@ def pick(n):
 def handle_message(event):
     text = event.message.text
     if re.match('[哈呵嘿嘻]', text) or re.match('XD+', text):
-        message = TextSendMessage(text="\u100079")
+        message = TextSendMessage(text="\U00100079")
     if re.match('.*常見問題', text):
         message = get_basic_ques()
     elif re.match('.*自我介紹', text):
         message = TextSendMessage(text="我從資訊技術出發，遊走於心理、設計與管理之間，致⼒於將以⼈為本的設計原則注入科技領域，並應⽤科技解決問題。\U0010008F")
     elif re.match('.*為什麼.*錄取', text):
-        message = TextSendMessage(text="好的工程師除了要有紮實的技術硬底子，也需要有良好的團隊合作能力。在資：管 = 7:3系上課程和課堂外project的訓練下必備的能力我不會少，而我還有很多社團和校外組織的跨領域合作經驗，在溝通和表達上更具優勢。\u100033")
+        message = TextSendMessage(text="好的工程師除了要有紮實的技術硬底子，也需要有良好的團隊合作能力。在資：管 = 7:3系上課程和課堂外project的訓練下必備的能力我不會少，而我還有很多社團和校外組織的跨領域合作經驗，在溝通和表達上更具優勢。\U00100033")
     elif re.match('.*為什麼.*申請', text):
-        message = TextSendMessage(text="Line不但是台灣市佔率最高的通訊軟體，也是積極開拓新服務和跨國網路和軟體公司，我很想試試和國際級的人才並肩工作的感覺！\u1000A5")
+        message = TextSendMessage(text="Line不但是台灣市佔率最高的通訊軟體，也是積極開拓新服務和跨國網路和軟體公司，我很想試試和國際級的人才並肩工作的感覺！\U001000A5")
     elif re.match('.*興趣', text):
         message = TextSendMessage(text="唱歌吧，你還可以問問我喜歡的歌手、運動、動物，或參加過的社團")
     elif re.match('[^不無]*[最喜歡愛]', text):
@@ -58,7 +58,7 @@ def handle_message(event):
         elif re.match('.*運動', text):
             message = get_favor('sport')
         else:
-            message = TextSendMessage(text="其他喜歡的東西還沒有加入喔～\u100065")
+            message = TextSendMessage(text="其他喜歡的東西還沒有加入喔～\U00100065")
     elif re.match('.*社團', text):
         message = get_clubs()
     elif re.match('.*技能', text):
